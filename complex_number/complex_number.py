e13557c415c2e3df6b06414bfa421e797b3296b4
Joost van Lingen <joost.vanlingen@evbox.com>
2024-03-13 17:57:55 +0100

    Added: Core operations for complex numbers

--- complex_number/complex_number.py
+++ complex_number/complex_number.py
@@ -1,23 +1,29 @@
 class ComplexNumber:
-    def __init__(self, real, imaginary):
+    def __init__(self, real=0.0, imaginary=0.0):
         self.real = real
         self.imaginary = imaginary
 
     def __repr__(self):
-        return f"ComplexNumber({self.real}, {self.imaginary})"
+        representation = f"{self.imaginary}j"
+        if self.real != 0:
+            representation = (
+                f"({self.real}{'+' if self.imaginary >= 0 else ''}{representation})"
+            )
+        return representation
 
     def __add__(self, other):
-        # Implement addition here
-        pass
+        return ComplexNumber(self.real + other.real, self.imaginary + other.imaginary)
 
     def __sub__(self, other):
-        # Implement subtraction here
-        pass
+        return ComplexNumber(self.real - other.real, self.imaginary - other.imaginary)
 
     def __mul__(self, other):
-        # Implement multiplication here
-        pass
+        real = self.real * other.real - self.imaginary * other.imaginary
+        imaginary = self.imaginary * other.real + self.real * other.imaginary
+        return ComplexNumber(real, imaginary)
 
     def __truediv__(self, other):
-        # Implement division here
-        pass
+        denom = other.real**2 + other.imaginary**2
+        real = (self.real * other.real + self.imaginary * other.imaginary) / denom
+        imaginary = (self.imaginary * other.real - self.real * other.imaginary) / denom
+        return ComplexNumber(real, imaginary)
