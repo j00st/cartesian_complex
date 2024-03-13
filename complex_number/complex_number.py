02f09625d729b2ee9f2586c3bd1c0686569b932e
Joost van Lingen <joost.vanlingen@evbox.com>
2024-03-13 16:05:54 +0100

    Initial commit

---
+++ complex_number/complex_number.py
@@ -0,0 +1,23 @@
+class ComplexNumber:
+    def __init__(self, real, imaginary):
+        self.real = real
+        self.imaginary = imaginary
+
+    def __repr__(self):
+        return f"ComplexNumber({self.real}, {self.imaginary})"
+
+    def __add__(self, other):
+        # Implement addition here
+        pass
+
+    def __sub__(self, other):
+        # Implement subtraction here
+        pass
+
+    def __mul__(self, other):
+        # Implement multiplication here
+        pass
+
+    def __truediv__(self, other):
+        # Implement division here
+        pass
