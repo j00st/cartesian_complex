602bbf7d1dc71036be521eaf05d41034f2ccae77
Joost van Lingen <joost.vanlingen@evbox.com>
2024-03-13 18:46:58 +0100

    Added: Support for real numbers in core operations

--- complex_number/complex_number.py
+++ complex_number/complex_number.py
@@ -4,26 +4,57 @@ class ComplexNumber:
         self.imaginary = imaginary
 
     def __repr__(self):
-        representation = f"{self.imaginary}j"
-        if self.real != 0:
-            representation = (
-                f"({self.real}{'+' if self.imaginary >= 0 else ''}{representation})"
-            )
-        return representation
+        if self.imaginary == 0:
+            return f"{self.real}"
+        elif self.real == 0:
+            return f"{self.imaginary}j"
+        else:
+            return f"({self.real}{'+' if self.imaginary >= 0 else ''}{self.imaginary}j)"
 
     def __add__(self, other):
-        return ComplexNumber(self.real + other.real, self.imaginary + other.imaginary)
+        if isinstance(other, ComplexNumber):
+            real = self.real + other.real
+            imaginary = self.imaginary + other.imaginary
+        elif isinstance(other, (int, float)):
+            real = self.real + other
+            imaginary = self.imaginary
+        else:
+            return NotImplemented
+        return ComplexNumber(real, imaginary)
 
     def __sub__(self, other):
-        return ComplexNumber(self.real - other.real, self.imaginary - other.imaginary)
+        if isinstance(other, ComplexNumber):
+            real = self.real - other.real
+            imaginary = self.imaginary - other.imaginary
+        elif isinstance(other, (int, float)):
+            real = self.real - other
+            imaginary = self.imaginary
+        else:
+            return NotImplemented
+        return ComplexNumber(real, imaginary)
 
     def __mul__(self, other):
-        real = self.real * other.real - self.imaginary * other.imaginary
-        imaginary = self.imaginary * other.real + self.real * other.imaginary
+        if isinstance(other, ComplexNumber):
+            real = self.real * other.real - self.imaginary * other.imaginary
+            imaginary = self.imaginary * other.real + self.real * other.imaginary
+        elif isinstance(other, (int, float)):
+            real = self.real * other
+            imaginary = self.imaginary * other
+        else:
+            return NotImplemented
         return ComplexNumber(real, imaginary)
 
     def __truediv__(self, other):
-        denom = other.real**2 + other.imaginary**2
-        real = (self.real * other.real + self.imaginary * other.imaginary) / denom
-        imaginary = (self.imaginary * other.real - self.real * other.imaginary) / denom
+        if isinstance(other, ComplexNumber):
+            denom = other.real**2 + other.imaginary**2
+            real = (self.real * other.real + self.imaginary * other.imaginary) / denom
+            imaginary = (
+                self.imaginary * other.real - self.real * other.imaginary
+            ) / denom
+        elif isinstance(other, (int, float)):
+            denom = other**2
+            real = self.real * other / denom
+            imaginary = self.imaginary * other / denom
+        else:
+            return NotImplemented
         return ComplexNumber(real, imaginary)
